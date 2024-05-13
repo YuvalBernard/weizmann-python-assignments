fe1ea78fb4f781b5da61d471f37ee5ca9ebd8d6b
Yuval Bernard <119048065+YuvalBernard@users.noreply.github.com>
2024-05-13 19:25:10 +0000

    Update hw4.py

--- day04/hw4.py
+++ day04/hw4.py
@@ -4,8 +4,8 @@ filename = sys.argv[1]
 with open(filename) as f:
     text = f.read().splitlines() # get list of lines
 
-lines = len(text)
-words = sum(len(line.split()) for line in text)
-characters = sum(len(line) for line in text)
+n_lines = len(text)
+n_words = sum(len(line.split()) for line in text)
+n_characters = sum(len(line) for line in text)
 
-print(f"File '{filename}' contains {lines} lines, {words} words, and {characters} characters.")
+print(f"File '{filename}' contains {n_lines} lines, {n_words} words, and {n_characters} characters.")
